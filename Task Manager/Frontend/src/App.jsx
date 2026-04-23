76d694444272f27b09e3ea8fd3b949181cb52759
amansinh-30 <amansinhparmar30@gmail.com>
2026-04-23 15:14:28 +0530

    Start working on new project

---
+++ Task Manager/Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="text-4xl text-center">
+        <h2 className="text-purple-600">Task Task Manager</h2>
+      </div>
+    </>
+  );
+};
+
+export default App;
