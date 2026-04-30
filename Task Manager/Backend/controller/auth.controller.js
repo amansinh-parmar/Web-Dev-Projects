a46e290558e9b5101be21243b39547a31c21d05d
amansinh-30 <amansinhparmar30@gmail.com>
2026-04-30 16:17:24 +0530

    sign-in route and auth controller

--- Task Manager/Backend/controller/auth.controller.js
+++ Task Manager/Backend/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../Models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { name, email, profileImgUrl, password, adminJoinCode } = req.body;
@@ -44,6 +45,37 @@ export const signup = async (req, res, next) => {
     await newUser.save();
 
     res.json("Signup Successful");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, nex) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password || email === "" || password === "") {
+      next(errorHandler(400, "All fields are required"));
+    }
+
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+
+    // Compare Password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(400, "Wrong Credentials"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const {password: pass, ...rest} = validUser._doc
+
+    res.status(200).cookie('access_token', token, {httpOnly: true}).json(rest)
   } catch (error) {
     next(error);
   }
--- Task Manager/Backend/routes/auth.route.js
+++ Task Manager/Backend/routes/auth.route.js
@@ -1,8 +1,9 @@
-import express from 'express'
-import { signup } from '../controller/auth.controller.js'
+import express from "express";
+import { signin, signup } from "../controller/auth.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/signup', signup)
+router.post("/sign-up", signup);
+router.post("/sign-in", signin);
 
-export default router
+export default router;
